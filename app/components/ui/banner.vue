70f661154de50b6b4275073fb3cc85f786504167
Maher Abaza <maher.abaza@protonmail.com>
2026-03-02 02:50:19 +0300

    fix: circassian genocide years

--- app/components/ui/banner.vue
+++ app/components/ui/banner.vue
@@ -11,7 +11,7 @@
       target="_blank"
       class="ms-auto me-2 p-2 flex items-center gap-2 text-[unset]"
     >
-      <Icon name="tabler:ribbon-health" /> <span>1764 - 1864</span>
+      <Icon name="tabler:ribbon-health" /> <span>1763 - 1864</span>
     </a>
   </div>
 </template>
