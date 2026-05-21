b4a695a45f8a1ea47f7b0f57548cea93776a9cda
Maher Abaza <maher.abaza@protonmail.com>
2026-05-21 17:47:22 +0300

    style(link-preview): improve visibility in light mode

--- app/components/link-preview.vue
+++ app/components/link-preview.vue
@@ -2,7 +2,7 @@
   <a
     :href="url"
     target="_blank"
-    class="flex flex-col max-w-120 rounded-md border border-grey-300 hover:border-grey-400 dark:border-grey-700 dark:hover:border-grey-600 text-inherit decoration-0 overflow-hidden"
+    class="flex flex-col max-w-120 rounded-md border border-grey-400 hover:border-grey-500 dark:border-grey-700 dark:hover:border-grey-600 text-inherit decoration-0 overflow-hidden"
   >
     <img
       v-if="image"
@@ -13,7 +13,7 @@
     <div class="flex flex-col gap-1 p-4">
       <span class="text-xs text-primary uppercase">{{ domain }}</span>
       <strong>{{ title }}</strong>
-      <p class="text-grey-400/50 line-clamp-2 overflow-hidden">
+      <p class="dark:text-grey-400/50 line-clamp-2 overflow-hidden">
         {{ description }}
       </p>
     </div>
